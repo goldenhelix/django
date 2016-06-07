bfb5ec5f215d41fad44f4830098261a262d48a8b
Nathan Fortier <fortier@goldenhelix.com>
2016-06-07 15:04:26 -0600

    Case 9806: Add timeouts to posgres queries

--- django/db/backends/postgresql_psycopg2/base.py
+++ django/db/backends/postgresql_psycopg2/base.py
@@ -184,6 +184,8 @@ class DatabaseWrapper(BaseDatabaseWrapper):
                 conn_params['host'] = settings_dict['HOST']
             if settings_dict['PORT']:
                 conn_params['port'] = settings_dict['PORT']
+            if settings_dict['QUERY_TIMEOUT']:
+                conn_params['options'] = '-c statement_timeout=' + settings_dict['QUERY_TIMEOUT']
             self.connection = Database.connect(**conn_params)
             self.connection.set_client_encoding('UTF8')
             tz = 'UTC' if settings.USE_TZ else settings_dict.get('TIME_ZONE')
